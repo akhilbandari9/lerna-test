51e2ffd179ee71e86f6883037aac5c0d7f1119a1
akhil-pc <akhilbandari9999@gmail.com>
2022-08-11 19:39:50 +0530

    workspace setup - lerna monorepo

---
+++ packages/shared/src/components/Button.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import {Button as MuiButton} from '@mui/material'
+
+export default function Button({children}) {
+	return <MuiButton>{children}</MuiButton>
+}
---
+++ packages/ums/src/App.tsx
@@ -0,0 +1,14 @@
+import {Button} from '@campx/shared'
+import {ThemeProvider} from '@mui/material'
+import React from 'react'
+import './App.css'
+
+function App() {
+	return (
+		<div className='App'>
+			<Button>Hello</Button>
+		</div>
+	)
+}
+
+export default App
